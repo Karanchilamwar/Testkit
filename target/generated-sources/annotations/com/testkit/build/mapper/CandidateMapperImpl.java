44a6caddf1952d5158662372e559a8317aeb2d71
Karan <karanchilamwar@gmail.com>
2020-06-19 09:52:51 +0530

    TK-02 feat:add exception handling changes

--- target/generated-sources/annotations/com/testkit/build/mapper/CandidateMapperImpl.java
+++ target/generated-sources/annotations/com/testkit/build/mapper/CandidateMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-06-18T13:04:19+0530",
+    date = "2020-06-18T13:54:38+0530",
     comments = "version: 1.4.0.Beta1, compiler: Eclipse JDT (IDE) 3.21.0.v20200304-1404, environment: Java 1.8.0_251 (Oracle Corporation)"
 )
 @Component
